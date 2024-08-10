370fc3a684670bb4b4289536014732cf344e9ebd
Sahil Yadav <sahilyadavind0908@gmail.com>
2024-08-10 18:58:32 +0530

    commit 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <></>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
